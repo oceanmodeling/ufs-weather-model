98a2981e56cec627968b4b38f79196d49e9af2d4
Panagiotis Velissariou <panagiotis.velissariou@noaa.gov>
2023-09-13 23:43:14 -0500

    added the PaHM model interface (initial config)

---
+++ PAHM-interface/cmake/macros.cmake
@@ -0,0 +1,12 @@
+# adds netcdf support
+macro(add_netcdf TARGET)
+  if(NetCDF_Fortran_FOUND)
+    target_compile_definitions(${TARGET} PRIVATE ADCNETCDF)
+    if(NetCDF4_FOUND)
+      target_compile_definitions(${TARGET} PRIVATE HAVE_NETCDF4)
+      target_compile_definitions(${TARGET} PRIVATE NETCDF_CAN_DEFLATE)
+    endif()
+    target_include_directories(${TARGET} PRIVATE ${NetCDF_Fortran_INCLUDE_DIRS})
+    target_link_libraries(${TARGET} PUBLIC NetCDF::NetCDF_Fortran)
+  endif()
+endmacro()
